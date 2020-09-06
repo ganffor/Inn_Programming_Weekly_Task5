d2edbb7038a4643bd2f59c101de517f5df1c406a
ganffor <69429261+ganffor@users.noreply.github.com>
2020-09-06 22:20:20 +0200

    Add project files.

---
+++ Inn_Programming_Weekly_Task5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+
+
+char input;
+
+int main()
+{
+    std::cout << "Enter a single character:" << std::endl;
+    std::cin >> input;
+
+
+    std::cout << "     1   2   3   4   5   6     " << std::endl;
+    std::cout << "    --- --- --- --- --- ---    " << std::endl;
+    std::cout << "A  | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " |  A" << std::endl;
+    std::cout << "    --- --- --- --- --- ---    " << std::endl;
+    std::cout << "B  | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " |  B" << std::endl;
+    std::cout << "    --- --- --- --- --- ---    " << std::endl;
+    std::cout << "C  | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " |  C" << std::endl;
+    std::cout << "    --- --- --- --- --- ---    " << std::endl;
+    std::cout << "D  | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " | " << "\033[31m" << input << "\033[37m" << " |  D" << std::endl;
+    std::cout << "    --- --- --- --- --- ---    " << std::endl;
+    std::cout << "     1   2   3   4   5   6     " << std::endl;
+}
